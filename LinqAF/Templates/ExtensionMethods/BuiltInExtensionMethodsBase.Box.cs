ebc47a726390f89f1809fd42a4589a8432824378
kevin-montrose <kevin.montrose@gmail.com>
2018-01-09 23:20:05 -0500

    boxing refinements

---
+++ LinqAF/Templates/ExtensionMethods/BuiltInExtensionMethodsBase.Box.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace LinqAF
+{
+    abstract partial class BuiltInExtensionMethodsBase
+    {
+        public BoxedEnumerable<TItem> Box<TItem>(BuiltInEnumerable<TItem> source)
+        {
+            if (((object)source) == null) return Impl.EmptyCache<TItem>.BadBoxedEmpty;
+
+            var bridge = Bridge(source, nameof(source));
+
+            return (BoxedEnumerable<TItem>)bridge;
+        }
+    }
+}
