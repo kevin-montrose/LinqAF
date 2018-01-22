ce2e9a720462f9e41718c0c9747d41e369bb49f7
kevin-montrose <kevin.montrose@gmail.com>
2018-01-22 11:42:14 -0500

    1.2.0

--- LinqAF/Properties/AssemblyInfo.cs
+++ LinqAF/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QuickLinq")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LinqAF")]
+[assembly: AssemblyDescription("Low allocation re-implementation of LINQ-to-Objects")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QuickLinq")]
+[assembly: AssemblyProduct("LinqAF")]
 [assembly: AssemblyCopyright("Copyright © 2017 - 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly:InternalsVisibleTo("LinqAF.Tests")]
