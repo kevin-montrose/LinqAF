7d46871d974036f5417c714479022a7a86e23437
kevin-montrose <kevin.montrose@gmail.com>
2018-01-21 20:52:31 -0500

    adding a .NET 3.5 build

--- LinqAF/Impl/CommonImplementation.ToList.cs
+++ LinqAF/Impl/CommonImplementation.ToList.cs
@@ -1,9 +1,5 @@
 ﻿using LinqAF.Config;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LinqAF.Impl
 {
--- LinqAF/Impl/IOfType.cs
+++ LinqAF/Impl/IOfType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LinqAF.Impl
+﻿namespace LinqAF.Impl
 {
     interface IOfType<TInItem, TThisEnumerable, TThisEnumerator>
         where TThisEnumerable : struct, IStructEnumerable<TInItem, TThisEnumerator>
